dcd042be103ae0f7cebe45e4531775c39f0485de
malleshbelavadigaviyappa <malleshbelavadigaviyappa@gmail.com>
2021-01-04 00:43:48 +0530

    handled sign in with new email and password in sign-in component

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -4,7 +4,7 @@ import FormInput from '../form-input/form-input.component';
 
 import CustomButton from '../custom-button/custom-button.component';
 
-import { signInWithGoogle } from '../../firebase/firebase.utils';
+import { auth, signInWithGoogle } from '../../firebase/firebase.utils';
 
 import './sign-in.styles.scss';
 
@@ -12,20 +12,28 @@ class SignIn extends React.Component {
 
     constructor(props) {
         super(props);
-
+        
         this.state = {
             email:'',
             password:''
         }
     }
 
-    handleSubmit = (event) => {
+    handleSubmit = async (event) => {
         event.preventDefault();
 
-        this.setState({
-            email:'',
-            password:''
-        }); 
+        const { email, password } = this.state;
+        try {
+            await auth.signInWithEmailAndPassword(email, password);
+
+            this.setState({
+                email:'',
+                password:''
+            }); 
+
+        } catch(error) {
+            console.log(error);
+        }
     }
 
     handleChange = (event) => {
